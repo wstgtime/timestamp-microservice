b3f6e7f1f0d3c1ed38fbacd216386137e973b376
wstgtime <denk174286@outlook.com>
2024-02-26 20:48:43 +0800

    add route for getting date using either date or utc string

--- index.js
+++ index.js
@@ -18,13 +18,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// Test api
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+function isValidDate(input) {
+  const outputDate = new Date(input);
+  return outputDate instanceof Date && !isNaN(outputDate);
+}
 
+function getUnixTime(input) {
+  return Math.floor(input / 1000);
+}
+
+// get date in milliseconds
+app.get("/api/:date?", function(req, res) {
+  const outputDate = isValidDate(new Date(req.params.date)) ? new Date(req.params.date) 
+    : isValidDate(new Date(parseInt(req.params.date))) ? new Date(parseInt(req.params.date)) 
+    : null;
+
+  if (!!outputDate) {
+    res.json({ unix: getUnixTime(outputDate), utc: outputDate.toUTCString() });
+  } else {
+    res.json({ res: 'invalid date' });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
